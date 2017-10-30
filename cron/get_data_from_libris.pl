95fcb1d7333ddcb2f17875232d8eeead5c9c8030
Magnus Enger <magnus@libriotech.no>
2017-10-30 15:13:51 +0100

    Add script to get and display requests
    This is the beginning of a script that can be run regularly to fetch
    requests from Libris and store them in Koha for further processing.
    Currently, requests are retrieved and important information about
    them is displayed, but no attempt at saving them is made.

---
+++ cron/get_data_from_libris.pl
@@ -0,0 +1,185 @@
+#!/usr/bin/perl
+
+# Copyright 2017 Magnus Enger Libriotech
+
+=head1 NAME
+
+get_data_from_libris.pl - Cronjob to fetch updates from Libris.
+
+=head1 SYNOPSIS
+
+ sudo koha-shell -c "perl get_data_from_libris.pl -v" koha
+
+=cut
+
+use LWP;
+use LWP::UserAgent;
+use JSON qw( decode_json );
+use Getopt::Long;
+use Data::Dumper;
+use Template;
+use DateTime;
+use Pod::Usage;
+use Modern::Perl;
+binmode STDOUT, ":utf8";
+
+# Get options
+my ( $limit, $verbose, $debug ) = get_options();
+
+# my $data = get_recent_data();
+# We should probably use "incoming", but while developing it is more predictable
+# to use "recent". Change to this before using in a live setting:
+my $data = get_incoming_data(); 
+
+say "Found $data->{'count'} requests" if $verbose;
+
+foreach my $req ( @{ $data->{'ill_requests'} } ) {
+
+    say "* $req->{'request_id'} / $req->{'lf_number'}:  $req->{'title'} ($req->{'year'})";
+    say "\tAuthor:       $req->{'author'} / $req->{'imprint'} / $req->{'place_of_publication'}";
+    say "\tISBN/ISSN:    $req->{'isbn_issn'}";
+    say "\tRequest type: $req->{'request_type'}";
+    say "\tProc. time:   $req->{'processing_time'} ($req->{'processing_time_code'})";
+    say "\tDeliv. type:  $req->{'delivery_type'} ($req->{'delivery_type_code'})";
+    say "\tStatus:       $req->{'status'}";
+    say "\tXstatus:      $req->{'xstatus'}";
+    say "\tMessage:      $req->{'message'}";
+    
+    my $receiving_library = $req->{'receiving_library'};
+    say "\tReceiving library: $receiving_library->{'name'} ($receiving_library->{'library_code'})";
+
+    say "\tRecipients:";
+    my @recipients = @{ $req->{'recipients'} };
+    foreach my $recip ( @recipients ) {
+        print "\t\t$recip->{'library_code'} ($recip->{'library_id'}) | ";
+        print "Location: $recip->{'location'} | ";
+        if ( $recip->{'response'} ) {
+            print "Response: $recip->{'response'} ($recip->{'response_date'}) | ";
+        }
+        print "Active: $recip->{'is_active_library'}";
+        print "\n";
+    }
+
+}
+
+# SUBROUTINES
+
+sub get_incoming_data {
+
+    return get_data( 'illrequests', 'incoming' );
+
+}
+
+sub get_recent_data {
+
+    return get_data( 'illrequests', 'recent' );
+
+}
+
+sub get_data {
+
+    my ( $action, $what ) = @_;
+
+    my $base_url  = 'http://iller.libris.kb.se/librisfjarrlan/api';
+    my $sigil     = 'Hig';
+    my $libriskey = 'xyz';
+
+    # Create a user agent object
+    my $ua = LWP::UserAgent->new;
+    $ua->agent("Koha ILL");
+
+    # Create a request
+    my $url = "$base_url/$action/$sigil/$what";
+    say "Requesting $url" if $verbose;
+    my $request = HTTP::Request->new( GET => $url );
+    $request->header( 'api-key' => $libriskey );
+
+    # Pass request to the user agent and get a response back
+    my $res = $ua->request($request);
+
+    my $json;
+    # Check the outcome of the response
+    if ($res->is_success) {
+        $json = $res->content;
+    } else {
+        say $res->status_line;
+    }
+
+    unless ( $json ) {
+        say "No JSON!";
+        exit;
+    }
+
+    my $data = decode_json( $json );
+    if ( $data->{'count'} == 0 ) {
+        say "No data!";
+        exit;
+    }
+
+    say Dumper $data if $debug;
+
+    return $data;
+
+}
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-l, --limit>
+
+Only process the n first requests. Not implemented.
+
+=item B<-v --verbose>
+
+More verbose output.
+
+=item B<-d --debug>
+
+Even more verbose output.
+
+=item B<-h, -?, --help>
+
+Prints this help message and exits.
+
+=back
+
+=cut
+
+sub get_options {
+
+    # Options
+    my $limit      = '';
+    my $verbose    = '';
+    my $debug      = '';
+    my $help       = '';
+
+    GetOptions (
+        'l|limit=i'  => \$limit,
+        'v|verbose'  => \$verbose,
+        'd|debug'    => \$debug,
+        'h|?|help'   => \$help
+    );
+
+    pod2usage( -exitval => 0 ) if $help;
+
+    return ( $limit, $verbose, $debug );
+
+}
+
+=head1 AUTHOR
+
+Magnus Enger, <magnus [at] libriotech.no>
+
+=head1 LICENSE
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
