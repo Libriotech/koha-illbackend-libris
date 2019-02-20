7c1b8f72e37d5684fd42a78508d501aa8d8595dd
Magnus Enger <magnus@libriotech.no>
2019-02-20 14:25:01 +0100

    Update ILL-status based on item being returned
    Fixes #58

--- cron/circstatus2illstatus.pl
+++ cron/circstatus2illstatus.pl
@@ -13,33 +13,42 @@ use C4::Context;
 my $dbh = C4::Context->dbh;
 my $ill_config = C4::Context->config('interlibrary_loans');
 my $sg = Koha::Illbackends::Libris::Base::status_graph();
-my $old_status = 'IN_ANK';
-my $new_status = 'IN_UTL';
-my $old_status_name = $sg->{ $old_status }->{ 'name' };
-my $new_status_name = $sg->{ $new_status }->{ 'name' };
 
-my $old_requests = Koha::Illrequests->search({ status => $old_status });
-while ( my $req = $old_requests->next ) {
+my %statuses = (
+    'IN_ANK' => 'IN_UTL',
+    'IN_UTL' => 'IN_RET'
+);
 
-    my $borrowernumber = $req->borrowernumber;
-    my $biblionumber   = $req->biblio_id;
+# Status pairs
+foreach my $old_status ( keys %statuses ) {
 
-    my $on_loan = $dbh->selectrow_hashref( 'SELECT issue_id FROM issues WHERE borrowernumber = ? AND itemnumber IN ( SELECT itemnumber FROM items WHERE biblionumber = ? )', undef, $borrowernumber, $biblionumber );
-    my $updated = '';
-    if ( $on_loan ) {
-        # Do the actual update
-        $req->status( $new_status );
-        $req->store;
-        $updated = $new_status;
-        # Add a comment
-        my $comment = Koha::Illcomment->new({
-            illrequest_id  => $req->illrequest_id,
-            borrowernumber => $ill_config->{ 'libris_borrowernumber' },
-            comment        => "Status ändrad från $old_status_name till $new_status_name.",
-        });
-        $comment->store();
+    my $new_status = $statuses{ $old_status };
+    my $old_status_name = $sg->{ $old_status }->{ 'name' };
+    my $new_status_name = $sg->{ $new_status }->{ 'name' };
+
+    my $old_requests = Koha::Illrequests->search({ status => $old_status });
+    while ( my $req = $old_requests->next ) {
+
+        my $borrowernumber = $req->borrowernumber;
+        my $biblionumber   = $req->biblio_id;
+
+        my $on_loan = $dbh->selectrow_hashref( 'SELECT issue_id FROM issues WHERE borrowernumber = ? AND itemnumber IN ( SELECT itemnumber FROM items WHERE biblionumber = ? )', undef, $borrowernumber, $biblionumber );
+        my $updated = '';
+        if ( ( $old_status eq 'IN_ANK' && $on_loan ) OR ( $old_status eq 'IN_UTL' && !$on_loan ) ) {
+            # Do the actual update
+            $req->status( $new_status );
+            $req->store;
+            $updated = $new_status;
+            # Add a comment
+            my $comment = Koha::Illcomment->new({
+                illrequest_id  => $req->illrequest_id,
+                borrowernumber => $ill_config->{ 'libris_borrowernumber' },
+                comment        => "Status ändrad från $old_status_name till $new_status_name.",
+            });
+            $comment->store();
+        }
+        say $req->illrequest_id . " $borrowernumber $biblionumber $updated";
+
     }
-    say $req->illrequest_id . " $borrowernumber $biblionumber $updated";
 
 }
-
