2e74b9042b39b0a33d5b8b338af4f25b1728f1ca
Magnus Enger <magnus@libriotech.no>
2023-02-23 12:39:17 +0100

    Start to add tools/check_config.pl

---
+++ tools/check_config.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+
+use Modern::Perl;
+use YAML::Syck qw( LoadFile );
+use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
+use Term::ANSIColor qw( :constants );
+
+use C4::Context;
+use Koha::Patrons;
+
+# Get the path to, and read in, the Libris ILL config file
+my $config_file = C4::Context->config('interlibrary_loans')->{'libris_config'};
+my $config = LoadFile( $config_file );
+my $checked_config = $config;
+
+say Dumper $config;
+
+# Patrons
+foreach my $var ( qw( unknown_patron libris_borrowernumber ) ) {
+
+    if ( defined $config->{ $var } ) {
+        my $patron = Koha::Patrons->find({ 'borrowernumber' => $config->{ $var } });
+        if ( $patron ) {
+            say GREEN, "OK, $var found: ", $patron->userid, RESET;
+        } else {
+            say RED, "$var not found!", RESET;
+        }
+    } else {
+        say RED, "$var not defined!";
+    }
+    delete $checked_config->{ $var };
+
+}
+
+# Patron ID fields
+foreach my $field ( qw( patron_id_field patron_id_field_alt ) ) {
+
+    if ( $config->{ $field } =~ m/^userid|cardnumber|borrowernumber$/ ) {
+        say GREEN, "OK, $field looks ok ($config->{ $field })", RESET;
+    } else {
+        say RED, "$field contains an illegal value: $config->{ $field }", RESET;
+    }
+    delete $checked_config->{ $field };
+
+}
+
+say "Not checked:";
+say Dumper $checked_config;
